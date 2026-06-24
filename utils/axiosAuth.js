39fc445cad6f6b4d154cc7659a75a9917547539d
TRIPLEHASH <168893404+TriplehashRanchi@users.noreply.github.com>
2026-06-24 17:28:48 +0530

    Added here api

--- utils/axiosAuth.js
+++ utils/axiosAuth.js
@@ -1,10 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import { STATIC_BASE_URL } from "../context/AuthContext";
 
-// Single source of truth: same host as AuthContext (see HOST there to switch
-// between local testing and production).
-export const API_BASE_URL = `${STATIC_BASE_URL}/api`;
+export const API_BASE_URL = "https://api.rmclub.co/api";
 
 // Global axios instance
 const axiosAuth = axios.create({
